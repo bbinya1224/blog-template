45a413f6d116c15133b662c9ed67849eebc10be7
bbinya1224 <bbinya1224@gmail.com>
2026-01-22 21:05:49 +0900

    modify: 코드리뷰 반영- before:content-[''] 추가

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -9,7 +9,7 @@ export function Skeleton({ className, ...props }: SkeletonProps) {
     <div
       className={cn(
         'relative overflow-hidden rounded-md bg-gray-200',
-        'before:absolute before:inset-0',
+        "before:absolute before:inset-0 before:content-['']",
         'before:-translate-x-full before:animate-[shimmer_2s_infinite]',
         'before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent',
         className
